4b9e714f97ca2417947b75b96ff9c7f8c30a1e43
MSebb23 <matteosabbatini23@gmail.com>
2021-03-03 18:08:21 +0100

    Aggiunto numero elemento inserito
    (funziona solo con l'input con la maiuscola)

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-array = [   "Bianchi", 
+// Array utenti 
+utenti = [  "Bianchi", 
             "Rossi", 
             "Duzioni", 
             "Balsano", 
             "Verdi"
         ];
-console.log(array.length);
-for (var i = 0; i < array.length; i++) {
-    document.getElementById("outcome").innerHTML += array[i] + "<br>";
+// Inserimento nuovo utente
+
+var new_utente = prompt("Inserisci il nuovo utente da inserire in lista")
+utenti.push(new_utente);
+
+console.log(utenti.length);
+
+// stampa array NON ordinato
+for (var i = 0; i < utenti.length; i++) {
+    document.getElementById("outcome").innerHTML += utenti[i] + "<br>";
 }
-array.sort();
-for (var i = 0; i < array.length; i++) {
-    document.getElementById("outcome").innerHTML += "<br>" + array[i];
+
+// ordina array
+utenti.sort();
+
+// stampa array ordinati
+for (var j = 0; j < utenti.length; j++) {
+    document.getElementById("outcome").innerHTML += "<br>" + utenti[j];
+}
+
+// stampa posizione nuovo array 
+for (var k = 0; k < utenti.length; k++) {
+    if (utenti[k] == new_utente) {
+        document.getElementById("order").innerHTML = (k + 1);
+    } 
 }
