e0c3050accec27c89d613fc1f5c5f419a2a22259
MSebb23 <matteosabbatini23@gmail.com>
2021-03-03 16:57:53 +0100

    Completed JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("potato");
+array = [   "Bianchi", 
+            "Rossi", 
+            "Duzioni", 
+            "Balsano", 
+            "Verdi"
+        ];
+console.log(array.length);
+for (var i = 0; i < array.length; i++) {
+    document.getElementById("outcome").innerHTML += array[i] + "<br>";
+}
+array.sort();
+for (var i = 0; i < array.length; i++) {
+    document.getElementById("outcome").innerHTML += "<br>" + array[i];
+}
